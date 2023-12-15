4a5166f1934821a5a94221df100ae92d7903b3da
Ismael Zaidi <i.zaidi@student.tudelft.nl>
2023-12-15 14:36:17 +0100

    added simple logic

--- krabby.py
+++ krabby.py
@@ -1,2 +1,39 @@
-import servokit from ServoKit
 import time
+from adafruit_servokit import ServoKit
+
+def initialize_servo_kits(channels_per_kit=16, kit1_address=None, kit2_address=None):
+    # Initialize the first servo kit (default I2C address if not provided)
+    kit1 = ServoKit(channels=channels_per_kit, address=kit1_address)
+
+    # Initialize the second servo kit with a specified I2C address (or default if not provided)
+    kit2 = ServoKit(channels=channels_per_kit, address=kit2_address)
+
+    return kit1, kit2
+
+def main():
+    # Set the desired I2C addresses for the servo kits
+    kit1_address = 0x40  # Use default I2C address for kit1
+    kit2_address = 0x50  # Specify I2C address for kit2
+
+    # Initialize servo kits
+    kit1, kit2 = initialize_servo_kits(kit1_address=kit1_address, kit2_address=kit2_address)
+
+    try:
+        # Set the angle for each servo to 90 degrees on both controllers
+        for i in range(16):
+            kit1.servo[i].angle = 90
+
+        # Set specific angles for servos on kit2
+        kit2.servo[0].angle = 90
+        kit2.servo[1].angle = 90
+        kit2.servo[2].angle = 0
+
+        # Add any additional operations or delays here
+
+    finally:
+        # Release resources
+        kit1.deinit()
+        kit2.deinit()
+
+if __name__ == "__main__":
+    main()
