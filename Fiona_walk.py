f6eff9564590fa1ee2eb59bca2bf8a99916ba851
Fiona <fiona.nagelhout@gmail.com>
2024-01-25 22:45:01 +0100

    added walk cycle

--- Fiona_walk.py
+++ Fiona_walk.py
@@ -34,6 +34,62 @@ def move(servo, degrees, kit = ServoKit(channels=16), kit2 = ServoKit(channels=1
     if servo <= 14:
         kit.servo[servo].angle = angle
     else:
-        servo = servo - 15
-        kit2.servo[servo].angle = angle
+        kit2.servo[servo - 15].angle = angle
 
+# do x amount of steps for this walk cycle
+def walk(x):
+    for i in range(x):
+        # set 1 up
+        move(1, 30)
+        move(10, 30)
+        move(13, 30)
+        time.sleep(0.5)
+        # push set 2
+        move(5, -20)
+        move(8, -20)
+        move(17, -20)
+        time.sleep(0.5)
+        # set 1 down
+        move(1, 0)
+        move(10, 0)
+        move(13, 0)
+        # set 2 up and place in middle
+        move(4, 30)
+        move(7, 30)
+        move(16, 30)
+        time.sleep(0.5)
+        move(5, 0)
+        move(8, 0)
+        move(17, 0)
+        # push set 1
+        move(2, -20)
+        move(11, -20)
+        move(14, -20)
+        # set 2 down
+        move(4, 0)
+        move(7, 0)
+        move(16, 0)
+        # place set 1 in middle
+        move(1, 30)
+        move(10, 30)
+        move(13, 30)
+        time.sleep(0.5)
+        move(2, 0)
+        move(11, 0)
+        move(14, 0)
+        # skip this step en repeat for continous walking
+        time.sleep(0.5)
+        move(1, 0)
+        move(10, 0)
+        move(13, 0)
+
+
+
+
+
+kit = ServoKit(channels=16)
+kit2 = ServoKit(channels=16, address=0x50)
+
+initialize_servos(kit, kit2)
+
+loose_servos(kit, kit2)
